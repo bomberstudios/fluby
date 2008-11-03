dd59f06ab5c87b038e4a14a43619fb6311ad08be
Ale Muñoz <bomberstudios@gmail.com>
2008-11-03 17:10:45 +0100

    Added nice colors to fluby output

--- lib/fluby.rb
+++ lib/fluby.rb
@@ -1,6 +1,18 @@
 module Fluby
   NAME    = 'fluby'
-  VERSION = '0.5.7'
+  VERSION = '0.5.8'
+
+  COLORS = {
+    :black => "\033[0;30m",
+    :red => "\033[0;31m",
+    :green => "\033[0;32m",
+    :yellow => "\033[0;33m",
+    :blue => "\033[0;34m",
+    :purple => "\033[0;35m",
+    :cyan => "\033[0;36m",
+    :white => "\033[0;37m",
+    :whitebold => "\033[1;37m"
+  }
 
   def self.create_project(name)
     require "fileutils"
@@ -10,10 +22,12 @@ module Fluby
     @project_name = name
     @project_folder = FileUtils.pwd + "/" + @project_name
 
+    puts "\n#{COLORS[:green]}Creating project #{@project_name}"
+
     # Make folders
     if File.exist? @project_folder
-      puts "Folder #{@project_name} already exists,"
-      puts "please choose a different name for your project"
+      puts "#{COLORS[:red]}Folder #{@project_name} already exists,"
+      puts "#{COLORS[:red]}please choose a different name for your project"
       exit
     end
     FileUtils.mkdir [@project_folder,"#{@project_folder}/deploy","#{@project_folder}/assets"]
@@ -38,6 +52,7 @@ module Fluby
       destination = source
     end
     FileUtils.cp "#{File.dirname(__FILE__)}/templates/#{source}", "#{@project_folder}/#{destination}"
+    log "#{@project_name}/#{destination}"
   end
 
   def self.render_template source, destination=nil
@@ -46,6 +61,21 @@ module Fluby
     end
     open("#{@project_folder}/#{destination}","w") do |f|
       f << ERB.new(IO.read("#{File.dirname(__FILE__)}/templates/#{source}")).result(binding)
+    end
+    log "#{@project_name}/#{destination}"
+  end
+
+  def self.log string
+    puts "    #{COLORS[:white]}Creating #{COLORS[:cyan]}#{string}#{COLORS[:white]}"
+  end
+
+  def self.in_textmate?
+    begin
+      if TextMate
+        return true
+      end
+    rescue
+      return false
     end
   end
 
